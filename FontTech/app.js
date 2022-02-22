7531b7e2b9d539b39df739645d2724526998bd8b
arielmartin33 <arielmartin33@gmail.com>
2022-02-22 20:26:29 -0300

    reparo error de logMiddleware

--- FontTech/app.js
+++ FontTech/app.js
@@ -18,10 +18,6 @@ app.use(cookies());
 
 app.use(userLoggedMiddleware);
 
-const logMiddleware = require('./middlewares/logMiddleware');
-
-
-
 app.use(express.static('./public'));
 app.use(express.urlencoded({ extended: false }));
 
@@ -36,7 +32,6 @@ const userRoutes = require('./routes/userRoutes');
 app.use('/', mainRoutes);
 app.use('/products', productRoutes);
 app.use('/user', userRoutes);
-app.use(logMiddleware);
 
 app.listen('3500',()=> console.log('Servidor corriendo en puerto 3500'));
 
---
+++ FontTech/models/Product.js
@@ -0,0 +1,60 @@
+// 1- Guardar usuario en la base de datos (json en este caso)
+// 2- Buscar usuario que se quiere loguear por su e-mail.
+// 3- BUscar a un usuario por su Id
+// 4- Editar la información de un usuario
+// 5- Eliminar un usuario de la bd
+const fs = require('fs');
+
+const Product = {
+
+    fileName: './data/productsDB.json',
+
+    getData: function () {
+        return JSON.parse(fs.readFileSync(this.fileName, 'utf-8'));
+    },
+// devuelvo todo el contenido del json
+
+    generateId: function () {
+        let allProduct = this.findAll();
+        let lastProduct = allProduct.pop(); // obtengo el ultimo producto
+        if (lastProduct){
+            return lastProduct.id + 1;
+        }
+        return 1;
+    },
+
+    findAll: function () {
+        return this.getData();
+    },
+
+    findByPk: function (id) {
+        let allProducts = this.findAll();
+        let productFound = allProducts.find(oneProduct => oneProduct.id === id);
+        return productFound;
+    },
+
+    findByField: function (field, text) {
+        let allProducts = this.findAll();
+        let productFound = allProducts.find(oneProduct => oneProduct[field] === text);
+        return productFound;
+    },
+
+    create: function (productData) {
+        let allProducts = this.findAll();
+        let newProduct = {
+            id: this.generateId(),
+            ...productData
+        }
+        allProducts.push(newProduct);
+        fs.writeFileSync(this.fileName, JSON.stringify(allProducts, null, ' '));
+        return newProduct;
+    },
+    delete: function (id) {
+        let allProduct = this.findAll();
+        let finalProducts = allProducts.filter(oneProduct => oneProduct.id !== id);
+        fs.writeFileSync(this.fileName, JSON.stringify(finalProducts, null, ' '));
+        return true;
+    }
+}
+
+module.exports = Product;
