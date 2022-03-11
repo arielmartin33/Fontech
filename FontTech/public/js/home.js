73eeeb0b56be9fcce5bae623b1a25b8e690af7c8
SanRRuiz <sandrarosanaruiz3@gmail.com>
2022-03-10 23:39:51 -0300

    trarando de agregar stylo desde el front

---
+++ FontTech/public/js/home.js
@@ -0,0 +1,8 @@
+window.addEventListener("load", function () {
+  let bottonNewsletter = document.getElementsByClassName("botonNewsletter");
+  console.log("funciona");
+
+  bottonNewsletter.addEventListener("click", function () {
+    alert("funciona");
+  });
+});
