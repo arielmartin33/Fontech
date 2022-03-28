d53574af3712a8b446b3d81fd5e7227b7fc295bf
SanRRuiz <sandrarosanaruiz3@gmail.com>
2022-03-28 20:59:09 -0300

    #

--- FontTech/database/config/config.js
+++ FontTech/database/config/config.js
@@ -1,23 +1,23 @@
 module.exports = {
-  "development": {
-    "username": "root",
-    "password": 'Fullstack2022',
-    "database": "fontechdb",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
+  development: {
+    username: "root",
+    password: "Fullstack2022",
+    database: "fontechdb",
+    host: "127.0.0.1",
+    dialect: "mysql",
   },
-  "test": {
-    "username": "root",
-    "password": 'Fullstack2022',
-    "database": "fontechdb",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
+  test: {
+    username: "root",
+    password: "Fullstack2022",
+    database: "fontechdb",
+    host: "127.0.0.1",
+    dialect: "mysql",
   },
-  "production": {
-    "username": "root",
-    "password": 'Fullstack2022',
-    "database": "fontechdb",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
-  }
-}
+  production: {
+    username: "root",
+    password: "Fullstack2022",
+    database: "fontechdb",
+    host: "127.0.0.1",
+    dialect: "mysql",
+  },
+};
